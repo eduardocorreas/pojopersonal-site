a4f39df6e6f784acb785d39c6487f227b17da13a
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-03-27 18:11:55 -0300

    Form to mailchimp

--- src/components/formMailchimp.js
+++ src/components/formMailchimp.js
@@ -1,51 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import addToMailchimp from 'gatsby-plugin-mailchimp'
 
-// import { Container } from './styles';
+import SocialIcons from './socialIcons'
 
 export default function FormMailchimp() {
-  return (
-    <div id="mc_embed_signup">
-      <form
-        action="https://pojopersonal.us19.list-manage.com/subscribe/post?u=31636fa43c0248e85123e0d93&amp;id=51f8f5b8c7"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        class="validate"
-        target="_blank"
-        novalidate
-      >
-        <div id="mc_embed_signup_scroll">
-          <input
-            type="email"
-            name="EMAIL"
-            className=" w-full sm:mx-auto bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-2/4 sm:w-2/4   appearance-none leading-normal placeholder-gray-600 text-gray-800"
-            id="mce-EMAIL"
-            placeholder="Digite o seu melhor e-mail"
-            required
-          />
+  const [email, setEmail] = useState('')
+  const [fname, setFname] = useState('')
+  const [displayIcons, setDisplayIcons] = useState('none')
+  const [displayForm, setDisplayForm] = useState('block')
 
-          <div
-            style={{ position: 'absolute', left: '-5000px' }}
-            aria-hidden="true"
-          >
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const result = await addToMailchimp(email, { FNAME: fname })
+    if (result.result === 'success') {
+      setFname('')
+      setEmail('')
+      setDisplayIcons('block')
+      setDisplayForm('none')
+    }
+  }
+
+  return (
+    <>
+      <div id="mc_embed_signup" style={{ display: displayForm }}>
+        <form onSubmit={e => handleSubmit(e)}>
+          <div id="mc_embed_signup_scroll">
             <input
               type="text"
-              name="b_31636fa43c0248e85123e0d93_51f8f5b8c7"
-              tabindex="-1"
-              value=""
+              name="fname"
+              value={fname}
+              onChange={e => setFname(e.target.value)}
+              className=" w-full mb-2 sm:mx-auto bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-2/4 sm:w-2/4   appearance-none leading-normal placeholder-gray-600 text-gray-800"
+              id="email"
+              placeholder="Qual é o seu nome?"
+              required
             />
-          </div>
-          <div class="clear mt-5">
             <input
-              type="submit"
-              value="FAZER DOWNLOAD DO E-BOOK GRÁTIS"
-              name="subscribe"
-              id="mc-embedded-subscribe"
-              class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="email"
+              name="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              className=" w-full sm:mx-auto bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-2/4 sm:w-2/4   appearance-none leading-normal placeholder-gray-600 text-gray-800"
+              id="email"
+              placeholder="Digite o seu melhor e-mail"
+              required
             />
+
+            <div
+              style={{ position: 'absolute', left: '-5000px' }}
+              aria-hidden="true"
+            >
+              <input
+                type="text"
+                name="b_31636fa43c0248e85123e0d93_51f8f5b8c7"
+                tabindex="-1"
+                value=""
+              />
+            </div>
+            <div class="clear mt-5">
+              <input
+                type="submit"
+                value="EU QUERO O E-BOOK GRÁTIS"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+                class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+              />
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      <div className="pt-5" style={{ display: displayIcons }}>
+        <h1 className="text-xl pb-5">
+          <span className="text-green-500 text-2xl">Parabéns, {fname}!</span>{' '}
+          <br />O seu e-mail foi cadastrado e logo mais você receberá o e-book
+          totalmente gratuito no seu e-mail!
+        </h1>
+        <p className="pb-5">
+          Quer mais conteúdos como esse? Siga-me nas redes sociais e fique por
+          dentro de todas as novidades ou faça parte da nossa Comunidade
+          Exclusiva no Telegram!
+        </p>
+        <SocialIcons />
+      </div>
+    </>
   )
 }
--- src/pages/ebook-mudanca-de-habito.js
+++ src/pages/ebook-mudanca-de-habito.js
@@ -1,19 +1,36 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Depoimentos from '../components/depoimentos'
 import FormMailchimp from '../components/formMailchimp'
 
 export default function EbookMudancaDeHabito() {
+  const data = useStaticQuery(graphql`
+    query {
+      ebook: file(relativePath: { eq: "ebook-cover-2.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <div className="h-screen bg-gray-900">
-      <div className=" pt-20 pb-20 sm:p-20 bg-gray-800 flex flex-col text-center text-white">
-        <div>
+      <div className=" pt-20 pb-20 sm:p-20 bg-gray-800 sm:flex text-center text-white">
+        <Img
+          fluid={data.ebook.childImageSharp.fluid}
+          className="w-2/4 sm:w-1/4 mx-auto mb-3"
+        />
+        <div className="self-center">
           <h1 className="text-xl sm:text-2xl px-5">
             Descubra Agora os 4 Pilares que levaram <br />
             <span className="text-green-500">
-              Mais de 30 Pessoas a uma Transformação na Saúde
-            </span>{' '}
-            <br />
+              vários alunos a uma Transformação na Saúde{' '}
+            </span>
             Apenas com a Mudança de Hábito!{' '}
           </h1>
           <div className="p-10">
@@ -21,6 +38,7 @@ export default function EbookMudancaDeHabito() {
           </div>
         </div>
       </div>
+
       <div>
         <Depoimentos />
       </div>
